6dafc3a4d8a60e14ff9b6769dcb569aa86c3db77
Christian <c2klele1@gmail.com>
2026-03-15 19:36:38 -0400

    Fix auth confirm route

--- frontend/app/auth/route.ts
+++ frontend/app/auth/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
+
   const code = searchParams.get("code");
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type");
